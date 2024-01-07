1dbaeeb9de119f36059845adf850412eeff8f0a1
SamirValizade-Gisman <93484165+SamirValizade-Gisman@users.noreply.github.com>
2024-01-07 23:56:02 +0400

    Welcome to the Band Name Generator

---
+++ Day-1.py
@@ -0,0 +1,8 @@
+# Welcome to the Band Name Generator
+
+print("Welcome to the Band Name Generator")
+city=input("What's name of the city you grew up in\n")
+pet_name=input("What's your pet's name\n")
+print("Your band name could be "+city+" "+pet_name)
+
+# Day 1 challange

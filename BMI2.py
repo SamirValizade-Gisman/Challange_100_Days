82d058a6c4f1d462f5b734032cfecac56a43fc4f
Samir Valizada Gisman <93484165+SamirValizade-Gisman@users.noreply.github.com>
2024-02-21 00:11:56 +0400

    BMI 2

---
+++ BMI2.py
@@ -0,0 +1,15 @@
+#BMI2
+print("Hello, Welcome check obese system")
+weight = int(input())  # in kilograms e.g., 72
+# Your code below this line 👇
+bmi = weight / (height * height)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")

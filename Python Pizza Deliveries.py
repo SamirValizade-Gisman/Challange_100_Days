3392e542ae38542668c8c0a0749429ba707ed6b3
Samir Valizada Gisman <93484165+SamirValizade-Gisman@users.noreply.github.com>
2024-02-22 00:25:30 +0400

    Python Pizza Deliveries 1.0

---
+++ Python Pizza Deliveries.py
@@ -0,0 +1,47 @@
+#Python Pizza Deliveries 1.0
+
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L?  ")
+add_pepperoni = input("Do you want pepperoni? Y or N?  ")
+extra_cheese = input("Do you want extra cheese? Y or N?  ")
+
+bill=0
+
+if size=="S":
+    bill=15
+    if add_pepperoni=="Y":
+        bill += 2
+    elif add_pepperoni=="N":
+        bill = bill
+    else:
+        bill=bill
+elif size=="M":
+    bill=20
+    if add_pepperoni=="Y":
+        bill += 3
+    elif add_pepperoni == "N":
+        bill = bill
+    else:
+        bill = bill
+elif size=="L":
+    bill=25
+    if add_pepperoni=="Y":
+        bill += 3
+    elif add_pepperoni == "N":
+        bill = bill
+    else:
+        bill = bill
+else:
+    bill=bill
+
+
+if extra_cheese=="Y":
+    bill+=1
+elif extra_cheese=="N":
+    bill=bill
+else:
+    bill=bill
+
+print(f"Your pizza bill {bill} dollar")
+

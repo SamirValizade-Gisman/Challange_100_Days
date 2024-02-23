3c0f14baeca4488509abac313a65e89dd6fcd2cc
Samir Valizada Gisman <93484165+SamirValizade-Gisman@users.noreply.github.com>
2024-02-23 21:20:40 +0400

    Update Welcome to the rollercoaster.py
    45 və 50 yaş arası yeni ayda əlavə ettim.

--- Welcome to the rollercoaster.py
+++ Welcome to the rollercoaster.py
@@ -11,8 +11,10 @@ if height >= 120:
   elif age <= 18:
     bill = 7
     print("Youth tickets are $7.")
+  elif:
+    age>45 and 50>age
+    print("You are free")
   else:
-    bill = 12
     print("Adult tickets are $12.")
 
   wants_photo = input("Do you want a photo taken? Y or N. ")
